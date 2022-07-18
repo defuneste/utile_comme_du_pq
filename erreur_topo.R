2bfa2b529edd84271e9eae77ee3cd2199a7efa6e
Olivier Leroy <leroy.oli@gmail.com>
2022-07-18 10:54:37 -0500

    up to 16

--- erreur_topo.R
+++ erreur_topo.R
@@ -4,11 +4,22 @@ library(sfheaders)
 # inspiration : 
 # http://s3.cleverelephant.ca/invalid.html
 
+## usefull functions 
+### function to convert sfheaders to sf and plot it
 quick_plot <- function(df){
     plot(sfheaders::sf_polygon(df))
 } 
 
-##  1. Exverted shell, point touch
+### scaling sfheader 
+
+scaling_sfheader <- function(df){
+    df_sf = sfheaders::sf_polygon(df)
+    df_centroid = sf::st_centroid(df_sf)
+    df_scale = (df_sf - df_centroid) * 0.8 + df_centroid
+        return(st_coordinates(df_scale$geometry)[,1:2])
+}
+
+##  1. Polygon - Exverted shell, point touch
 df <- data.frame(
 x = c(1, 1, 4, 8, 8, 4, 1),
 y = c(1, 8, 6, 1, 8, 6, 1)
@@ -16,7 +27,7 @@ y = c(1, 8, 6, 1, 8, 6, 1)
 
 quick_plot(df)
 
-##  2. Exverted shell, point-line touch
+##  2. Polygon - Exverted shell, point-line touch
 
 df <- data.frame(
     x = c(1, 1, 8, 8, 5, 1),
@@ -26,7 +37,7 @@ df <- data.frame(
 quick_plot(df)
 
 
-##  3. EExverted shell, line touch
+##  3. Polygon - Exverted shell, line touch
 
 df <- data.frame(
     x = c(1, 1, 8, 8, 5, 3, 1),
@@ -36,7 +47,7 @@ df <- data.frame(
 quick_plot(df)
 
 
-## 4. Inverted shell, point touch
+## 4. Polygon - Inverted shell, point touch
 
 df <- data.frame(
     x = c(1, 1, 4, 2, 6, 4, 8, 8, 1),
@@ -45,7 +56,7 @@ df <- data.frame(
 
 quick_plot(df)
 
-## 5. Inverted shell, point-line touch
+## 5. Polygon - Inverted shell, point-line touch
 
 df <- data.frame(
     x = c(1, 1, 3, 3, 7, 3, 8, 8, 1),
@@ -54,7 +65,7 @@ df <- data.frame(
 
 quick_plot(df)
 
-## 6. Inverted shell, line touch, exterior
+## 6. Polygon - Inverted shell, line touch, exterior
 
 df <- data.frame(
     x = c(1, 1, 6, 6, 3, 3, 8, 8, 1),
@@ -63,7 +74,7 @@ df <- data.frame(
 
 quick_plot(df)
 
-## 7. Inverted shell, line touch, interior
+## 7. Polygon - Inverted shell, line touch, interior
 
 df <- data.frame(
     x = c(1, 1, 4.5, 4.5, 3,   6,   4.5, 4.5, 8, 8, 1  ),
@@ -103,10 +114,63 @@ inner <- data.frame(
     y = c(1, 8, 8, 5, 5, 8, 8, 1, 1)
 )
 
-inner_sf <- sfheaders::sf_polygon(inner)
-inner_centroid <- sf::st_centroid(inner_sf)
-inner_scale <- (inner_sf - inner_centroid) * 0.8 + inner_centroid
+# scaling 
 
-# scaling
+p2 <- scaling_sfheader(inner)
 
-plot(sfheaders::sf_polygon(small_inner))
+pol <-st_polygon(list(p1,p2))
+plot(pol)
+
+## 12 Polygon/Hole - Inverted hole, point-line touch
+
+inner <- data.frame(
+    x = c(1, 1, 3, 3, 7, 3, 8, 8, 1),
+    y = c(1, 8, 8, 2, 2, 5, 5, 1, 1)
+)
+
+p2 <- scaling_sfheader(inner)
+
+pol <-st_polygon(list(p1,p2))
+plot(pol)
+
+
+## 13. Polygon/Hole - Inverted hole, line touch, interior
+
+inner <- data.frame(
+    x = c(1, 1, 6, 6, 3, 3, 8, 8, 1),
+    y = c(1, 8, 8, 4, 4, 8, 8, 1, 1)
+)
+
+p2 <- scaling_sfheader(inner)
+
+pol <-st_polygon(list(p1,p2))
+plot(pol)
+
+## 14. Polygon/Hole - Inverted hole, line touch, exterior 
+
+p2 <- rbind(c(2,2), c(7, 2), c(4.5, 7), c(4.5, 5), c(3.5, 3.5), c(5.5, 3.5),
+            c(4.5, 5), c(4.5, 7), c(2,2))
+
+pol <-st_polygon(list(p1,p2))
+plot(pol)
+
+## 15. Polygon/Hole - Exverted shell, point touch; exverted hole, point touch
+
+p1 <- rbind(c(1,1), c(1,8), c(8,1), c(8,8), c(1,1))
+p2 <- rbind(c(2,3), c(2,6), c(7,3), c(7,6), c(2,3))
+
+pol <-st_polygon(list(p1,p2))
+plot(pol)
+
+## 16. Polygon/Hole - Exverted shell, point touch; exverted hole, line touch 
+
+p2 <- rbind(c(2,3), c(2,6), c(3.5, 4.5), c(5.5, 4.5)
+            , c(7,3), c(7,6), 
+            c(5.5, 4.5), c(3.5, 4.5),
+            c(2,3))
+
+pol <-st_polygon(list(p1,p2))
+plot(pol)
+
+## 17. Polygon/Hole - Exverted shell, line touch; exverted hole, line touch 
+
